563fd79a34bb2fab1a61a92e3c42eab967732c33
vishaltyagicoding <124853710+vishaltyagicoding@users.noreply.github.com>
2023-02-11 08:15:22 +0530

    Add files via upload

---
+++ if_else_exercise.py
@@ -0,0 +1,32 @@
+
+# TODO 1
+# input the student percentage
+per = int(input("Enter your percentage? "))
+# TODO 2
+# If percentage of the student is more than
+# 80 then print A+
+if per > 80:
+    print("A+")
+
+# TODO 3
+# If percentage of the student is more than
+# 60 then print A
+elif per > 60:
+    print("A")
+# TODO 4
+# If percentage of the student is more than
+# 50 then print B
+elif per > 50:
+    print("B")
+
+# TODO 5
+# If percentage of the student is more than
+# 33 then print pass
+elif per > 33:
+    print("pass")
+
+# TODO 6
+# If percentage of the student is less than
+# 33 then print fail
+else:
+    print("fail")
